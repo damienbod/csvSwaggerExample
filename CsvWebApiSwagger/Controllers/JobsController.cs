3305918a33db0034ee14b69ed961668b8a3a7550
damienbod <damien_bod@hotmail.com>
2019-02-03 16:13:27 +0100

    creating a second API

---
+++ CsvWebApiSwagger/Controllers/JobsController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CsvWebApiSwagger.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsvWebApiSwagger.Controllers
+{
+    [Route("api/[controller]")]
+    public class JobsController : Controller
+    {
+        private static List<Job> Jobs = new List<Job>
+        {
+
+        };
+
+        /// <summary>
+        /// docs for get
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Job> Get()
+        {
+            return Jobs;
+        }
+
+        /// <summary>
+        /// get id
+        /// </summary>
+        /// <param name="id">any id</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public Job Get(int id)
+        {
+            return Jobs.First(j => j.Id == id);
+        }
+
+        /// <summary>
+        /// post a string
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody]Job value)
+        {
+            Jobs.Add(value);
+        }
+
+        /// <summary>
+        /// put a string
+        /// </summary>
+        /// <param name="id">id of a string</param>
+        /// <param name="value">value of the string</param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Job value)
+        {
+            var job = Jobs.First(j => j.Id == id);
+            job.Description = value.Description;
+            job.Level = value.Level;
+            job.Title = value.Title;
+            job.Requirements = value.Requirements;
+        }
+
+        /// <summary>
+        /// delete something
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var job = Jobs.First(j => j.Id == id);
+            Jobs.Remove(job);
+        }
+    }
+}
---
+++ CsvWebApiSwagger/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsvWebApiSwagger.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Level { get; set; }
+        public string Requirements { get; set; }
+    }
+}
