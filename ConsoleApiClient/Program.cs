f1f6233bdd49eb96fd87f051d59850acf4287450
damienbod <damien_bod@hotmail.com>
2019-02-12 16:59:29 +0100

    Adding api client

---
+++ ConsoleApiClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApiClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("begin...");
+
+            HttpClient httpClient = new HttpClient();
+            var clientCsvWebApiSwagger = new Client("https://localhost:44354/", httpClient);
+
+            // Gets all to-dos from the API
+            var all = await clientCsvWebApiSwagger.GetAllAsync();
+
+            Console.ReadLine();
+
+        }
+    }
+}
